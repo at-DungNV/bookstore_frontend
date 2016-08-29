a97206b17a34eeebd54bdd353ac285ca2d81f896
Dung Nguyen Van <nobinguyen94@gmail.com>
2016-08-29 10:16:46 +0700

    add feature login

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,6 +9,8 @@ bookControllers.controller('HomeController', ['$scope', '$route', 'myHttp',
     myHttp.index('http://bookstore.me/api/articles').then(
       function(successParam) { // success callback
         $scope.articles = successParam.data;
+        
+        console.log(successParam.data);
       }, function(rejectParam) { // error callback with reason
         console.log("rejected");
       }, function(notifyParam) { // notification
@@ -18,29 +20,50 @@ bookControllers.controller('HomeController', ['$scope', '$route', 'myHttp',
   }
 ]);
 
-bookControllers.controller('IndexController', ['$scope', '$route', 'myHttp', 
-  function ($scope, $route, myHttp) {
-    
+bookControllers.controller('IndexController', ['$scope', '$route', 'myHttp', '$localStorage', 
+  function ($scope, $route, myHttp, $localStorage) {
+  }
+]);
+
+bookControllers.controller('LogoutController', ['$scope', '$route', 'AuthenticationService', 
+  function ($scope, $route, AuthenticationService) {
+    AuthenticationService.logout();
   }
 ]);
 
 bookControllers.controller('AboutController', ['$scope', '$route', 'myHttp', 
   function ($scope, $route, myHttp) {
-    myHttp.index('http://bookstore.me/api/users').then(
-      function(successParam) { // success callback
-        $scope.data = successParam.data;
-      }, function(rejectParam) { // error callback with reason
-        console.log("rejected");
-      }, function(notifyParam) { // notification
-        console.log("notify");
-      }
-    );
-}]);
+    // myHttp.index('http://bookstore.me/api/users').then(
+    //   function(successParam) { // success callback
+    //     $scope.data = successParam.data;
+    //   }, function(rejectParam) { // error callback with reason
+    //     console.log("rejected");
+    //   }, function(notifyParam) { // notification
+    //     console.log("notify");
+    //   }
+    // );
+  }
+]);
 
-bookControllers.controller('LoginController', ['$scope', '$route',
-  function ($scope, $route) {
+bookControllers.controller('LoginController', ['$scope', '$location', '$rootScope', '$auth', '$state', 'AuthenticationService', 
+  function ($scope, $location, $rootScope, $auth, $state, AuthenticationService) {
+    $scope.email = 'dungnv@gmail.com';
+    $scope.password = '12345678';
+    var url = 'http://bookstore.me/api/authenticate';
     $scope.isShowedLogin = true;
     $scope.setShowedLogin = function (value) {
       $scope.isShowedLogin = value;
     };
+    $scope.login = function () {
+      AuthenticationService.login($scope.email, $scope.password, url).then(
+        function(successParam) { // success callback
+          // got error here, after succesfully login and logoutcannot send url immediately
+          $location.path('/home');
+        }, function(rejectParam) { // error callback with reason
+          $location.path('/login');
+        }, function(notifyParam) { // notification
+          console.log("notify");
+        }
+      );
+    }
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var bookServices = angular.module('bookServices', []);
+var shareServices = angular.module('shareServices', []);
 
-bookServices.service('myHttp', ['$http', '$q', function ($http, $q) {
+shareServices.service('myHttp', ['$http', '$q', '$localStorage', function ($http, $q, $localStorage) {
   this.index = function (url) {
     var deferred = $q.defer();
     return $http.get(url)
@@ -13,5 +13,39 @@ bookServices.service('myHttp', ['$http', '$q', function ($http, $q) {
         deferred.reject(data);
       }
     );
+  }
+}]);
+
+shareServices.service('AuthenticationService', ['$http', '$q', '$localStorage', '$location', function ($http, $q, $localStorage, $location) {
+  this.login = function (email, password, url) {
+    var deferred = $q.defer();
+    return $http.post(url, { email: email, password: password })
+      .success(function (response) {
+        if (response.token) {
+            // store username and token in local storage to keep user logged in between page refreshes
+            $localStorage.currentUser = { email: email, token: response.token };
+            // add jwt token to auth header for all requests made by the $http service
+            $http.defaults.headers.common.Authorization = 'Bearer ' + response.token;
+            console.log($http);
+        }
+        deferred.resolve(response);
+      })
+      .error(function (response) {
+        deferred.reject(response);
+      }
+    );
+  }
+  this.logout = function () {
+    delete $localStorage.currentUser;
+    $http.defaults.headers.common.Authorization = '';
+    $location.path('/login');
+  }
+}]);
+
+shareServices.service('checkLogin', ['$localStorage', '$location', function ($localStorage, $location) {
+  this.isLoggedIn = function () {
+    if(!$localStorage.currentUser) {
+      $location.path('/index');
+    }
   }
 }]);
